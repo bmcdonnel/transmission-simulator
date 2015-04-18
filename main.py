276ea64b94aef8317f65b03b28c48639dfc761ae
Bryan McDonnel <bryan@spotmac717.local>
2015-04-18 17:04:12 -0500

    adding initial components

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+import components.engine
+import components.torque_converter
+import components.transmission
+import components.vehicle_dynamics
+
+def main():
+  print "hello"
+
+if __name__ == "__main__":
+  main()
