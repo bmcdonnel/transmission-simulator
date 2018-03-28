0870bedc02433a6da8acda0546010e6a9a2514b8
Bryan McDonnel <bryan.mcdonnel@gmail.com>
2018-03-27 21:32:31 -0500

    add run.sh and bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# create a virtual environment
+rm -rf venv
+python3 -m venv venv
+source venv/bin/activate
+
+# these packages will be installed to the venv, and will not affect the system at large
+pip install pylint
+pip install nose
+pip install mock
+pip install matplotlib
